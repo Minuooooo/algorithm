dee97d6e90545f4c8e1aa044f58d619907737e42
Minuooooo <mmw7741@gmail.com>
2023-01-05 16:53:35 +0900

    Step06

---
+++ src/backjoon/step06/AlphabetFrequencyMain.java
@@ -0,0 +1,34 @@
+package backjoon.step06;
+
+import java.util.Scanner;
+
+public class AlphabetFrequencyMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.next();
+        word = word.toLowerCase();
+        int[] frequency = new int[26];
+        int max = 0;
+        int maxAlphabet = 0;
+        int ch = 0;
+
+        for (int i = 0; i < word.length(); i++)
+            frequency[word.charAt(i) - 'a']++;
+
+        for (int i = 0; i < 26; i++) {
+            if(frequency[i] > max) {
+                max = frequency[i];
+                maxAlphabet = i;
+                ch = i + 'A';
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if((i != maxAlphabet) && (frequency[i] == max))
+                ch = '?';
+        }
+
+        System.out.println((char)ch);
+    }
+}
---
+++ src/backjoon/step06/NumCompareMain.java
@@ -0,0 +1,23 @@
+package backjoon.step06;
+
+import java.util.Scanner;
+
+public class NumCompareMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+
+        int num1 = (str1.charAt(0) - '0') + 10 * (str1.charAt(1) - '0') + 100 * (str1.charAt(2) - '0');
+        int num2 = (str2.charAt(0) - '0') + 10 * (str2.charAt(1) - '0') + 100 * (str2.charAt(2) - '0');
+        int max = 0;
+
+        if(num1 > num2)
+            max = num1;
+        else
+            max = num2;
+
+        System.out.println(max);
+    }
+}
