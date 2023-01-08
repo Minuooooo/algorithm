eeae7bfe09ffabb9b67293b500b0bd2d33171a47
Minuooooo <mmw7741@gmail.com>
2023-01-08 15:30:27 +0900

    step08 End

--- src/backjoon/step08/PrimePracticeMain.java
+++ src/backjoon/step08/PrimePracticeMain.java
@@ -40,6 +40,9 @@ public class PrimePracticeMain {
         prime[1] = true;
 
         for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i])
+                continue;
+
             for(int j = i * i; j < prime.length; j += i)
                 prime[j] = true;
         }
