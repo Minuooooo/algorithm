95580dc6c49d7ebdf7ac49c6b4a0d48b1a3c9c0f
Minuooooo <mmw7741@gmail.com>
2023-06-25 09:24:06 +0900

    [ feat ] Bubble sort

---
+++ src/basic/sort/bubble/P2750_수정렬하기1.java
@@ -0,0 +1,30 @@
+package basic.sort.bubble;
+
+import java.util.Scanner;
+
+public class P2750_수정렬하기1 {  // Bubble sort -> O{n^2)
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        // Bubble sort 구현 영역
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if(A[j] > A[j + 1]) {  // swap
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
