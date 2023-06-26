1c46ea64bf709335c91a09bebcd7a0d9c682c372
Minuooooo <mmw7741@gmail.com>
2023-06-26 14:44:24 +0900

    [ feat ] 소수 구하기

--- src/basic/greedy/P1541_최솟값을만드는괄호배치.java
+++ src/basic/greedy/P1541_최솟값을만드는괄호배치.java
@@ -25,7 +25,7 @@ public class P1541_최솟값을만드는괄호배치 {
 
     private static int mySum(String s) {
         int sum = 0;
-        String[] temp = s.split("[+]");
+        String[] temp = s.split("[+]");  // split 함수가 동작하지 않는 문자는 []로 묶기
 
         for (int i = 0; i < temp.length; i++) {
             sum += Integer.parseInt(temp[i]);
