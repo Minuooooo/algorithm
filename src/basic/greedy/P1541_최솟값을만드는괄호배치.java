66f9e6a007a2b2f4aebd74d1e85b740a4c9f5cf1
Minuooooo <mmw7741@gmail.com>
2023-06-26 14:27:46 +0900

    [ feat ] 괄호를 배치하여 최솟값 찾기

---
+++ src/basic/greedy/P1541_최솟값을만드는괄호배치.java
@@ -0,0 +1,36 @@
+package basic.greedy;
+
+import java.util.Scanner;
+
+public class P1541_최솟값을만드는괄호배치 {
+
+    static int answer = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String example = scanner.nextLine();
+        String[] str = example.split("-");
+
+        for (int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+
+            if(i == 0)
+                answer += temp;
+            else
+                answer -= temp;
+        }
+        System.out.println(answer);
+    }
+
+    private static int mySum(String s) {
+        int sum = 0;
+        String[] temp = s.split("[+]");
+
+        for (int i = 0; i < temp.length; i++) {
+            sum += Integer.parseInt(temp[i]);
+        }
+
+        return sum;
+    }
+}
