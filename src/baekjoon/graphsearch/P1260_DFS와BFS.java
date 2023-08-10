f69f12ef63c681735252b94037390d76bd7e452f
Minuooooo <mmw7741@gmail.com>
2023-08-10 09:29:43 +0900

    [ feat ] P1260_DFS와BFS

--- src/baekjoon/graphsearch/P1260_DFS와BFS.java
+++ src/baekjoon/graphsearch/P1260_DFS와BFS.java
@@ -3,31 +3,37 @@ package baekjoon.graphsearch;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class P1260_DFS와BFS {
 
-    static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> graph;
-    static int N, M, V;
+    static List<List<Integer>> graph;
+    static boolean[] isVisited;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N + 1];
         graph = new ArrayList<>(N + 1);
+        isVisited = new boolean[N + 1];
 
         for(int i = 0; i < N + 1; i++) {
             graph.add(new ArrayList<>());
         }
 
         for(int i = 0; i < M; i++) {
+
             st = new StringTokenizer(br.readLine(), " ");
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
@@ -43,33 +49,39 @@ public class P1260_DFS와BFS {
         dfs(V);
         sb.append('\n');
 
-        visited = new boolean[N + 1];
+        isVisited = new boolean[N + 1];
         bfs(V);
 
         System.out.println(sb);
+
     }
-    static void dfs(int v) {
-        visited[v] = true;
+
+    public static void dfs(int v) {
+
         sb.append(v).append(" ");
+        isVisited[v] = true;
 
         for(int nextV : graph.get(v)) {
-            if(!visited[nextV])
+            if(!isVisited[nextV])
                 dfs(nextV);
         }
     }
-    static void bfs(int v) {
+
+    public static void bfs(int v) {
+
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(v);
-        visited[v] = true;
+        isVisited[v] = true;
 
-        while(!queue.isEmpty()) {
-            int temp = queue.poll();
-            sb.append(temp).append(" ");
+        while (!queue.isEmpty()) {
 
-            for(int nextV : graph.get(temp)) {
-                if(!visited[nextV]) {
+            int now = queue.poll();
+            sb.append(now).append(" ");
+
+            for(int nextV : graph.get(now)) {
+                if(!isVisited[nextV]) {
                     queue.offer(nextV);
-                    visited[nextV] = true;
+                    isVisited[nextV] = true;
                 }
             }
         }
