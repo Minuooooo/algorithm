add66d61f8c1b94ddd95b470f6d01dd686be6e15
Minuooooo <mmw7741@gmail.com>
2023-08-23 15:37:06 +0900

    [ feat ] P9663_NQueen

---
+++ src/baekjoon/backtracking/P9663_NQueen.java
@@ -0,0 +1,51 @@
+package baekjoon.backtracking;
+
+import java.util.Scanner;
+
+public class P9663_NQueen {
+
+    public static int[] arr;
+    public static int N;
+    public static int count = 0;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+        arr = new int[N];
+
+        nQueen(0);
+        System.out.println(count);
+    }
+
+    public static void nQueen(int depth) {
+
+        if(depth == N) {
+            count++;
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+
+            arr[depth] = i;
+
+            if(possibility(depth))
+                nQueen(depth + 1);
+        }
+    }
+
+    public static boolean possibility(int col) {
+
+        for(int i = 0; i < col; i++) {
+
+            if (arr[col] == arr[i])
+                return false;
+
+            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i]))
+                return false;
+        }
+
+        return true;
+    }
+}
