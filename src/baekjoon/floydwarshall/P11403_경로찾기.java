4dc112d2fff017f905b7179d9ce0ce61e8d32838
Minuooooo <mmw7741@gmail.com>
2023-09-25 11:20:26 +0900

    [ feat ] P11403_경로 찾기

---
+++ src/baekjoon/floydwarshall/P11403_경로찾기.java
@@ -0,0 +1,43 @@
+package baekjoon.floydwarshall;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P11403_경로찾기 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {  // k 노드를 거쳐서
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (arr[i][k] == 1 && arr[k][j] == 1)  // i -> k -> j
+                        arr[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
