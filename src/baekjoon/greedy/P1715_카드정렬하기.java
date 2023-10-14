a232579e021603a983198600e090ad3f7f63358b
Minuooooo <mmw7741@gmail.com>
2023-10-14 10:54:41 +0900

    [ feat ] P1715_카드 정렬하기

--- src/baekjoon/greedy/P1715_카드정렬하기.java
+++ src/baekjoon/greedy/P1715_카드정렬하기.java
@@ -7,23 +7,27 @@ import java.util.PriorityQueue;
 
 public class P1715_카드정렬하기 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Long> pq = new PriorityQueue<>();
 
-        for(int i = 0; i < N; i++) {
-            pq.add(Long.parseLong(br.readLine()));
+        PriorityQueue<Long> priorityQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            priorityQueue.offer(Long.parseLong(br.readLine()));
         }
 
         long sum = 0;
 
-        while(pq.size() > 1) {
-            long temp1 = pq.poll();
-            long temp2 = pq.poll();
+        while (priorityQueue.size() > 1) {
 
-            sum += temp1 + temp2;
-            pq.add(temp1 + temp2);
+            long temp1 = priorityQueue.poll();
+            long temp2 = priorityQueue.poll();
+
+            sum += (temp1 + temp2);
+
+            priorityQueue.offer(temp1 + temp2);
         }
 
         System.out.println(sum);
