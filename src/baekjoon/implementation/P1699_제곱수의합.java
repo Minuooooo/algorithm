ca069567ca2cda634c3510731371816d892ffe49
Minuooooo <mmw7741@gmail.com>
2023-10-20 10:00:40 +0900

    [ feat ] P1699_제곱수의 합

---
+++ src/baekjoon/implementation/P1699_제곱수의합.java
@@ -0,0 +1,28 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+public class P1699_제곱수의합 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] dp = new int[100001];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            dp[i] = i;
+
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+        scanner.close();
+    }
+}
