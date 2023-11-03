0033026b2e6bc7ef5e9c134a392d6598dcc8e141
Minuooooo <mmw7741@gmail.com>
2023-11-03 10:08:11 +0900

    [ feat ] P11728_배열 합치기

---
+++ src/baekjoon/twopointer/P11728_배열합치기.java
@@ -0,0 +1,67 @@
+package baekjoon.twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11728_배열합치기 {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        int[] B = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        concatenateAndSort(A, B);
+        System.out.println(sb);
+    }
+
+    private static void concatenateAndSort(int[] arr1, int[] arr2) {
+
+        int arr1Size = arr1.length;
+        int arr2Size = arr2.length;
+
+        int index1 = 0;
+        int index2 = 0;
+
+        while (index1 < arr1Size && index2 < arr2Size) {
+
+            if (arr1[index1] < arr2[index2])
+                sb.append(arr1[index1++]).append(" ");
+
+            else
+                sb.append(arr2[index2++]).append(" ");
+        }
+
+        if (index1 == arr1Size)
+            printRestData(arr2, index2, arr2Size);
+
+        else
+            printRestData(arr1, index1, arr1Size);
+    }
+
+    private static void printRestData(int[] biggerArr, int start, int end) {
+
+        for (int i = start; i < end; i++) {
+            sb.append(biggerArr[i]).append(" ");
+        }
+    }
+}
