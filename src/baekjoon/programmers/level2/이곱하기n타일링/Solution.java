6b1f84d2e477b75d04df5706df50318ea6096886
Minuooooo <mmw7741@gmail.com>
2024-02-11 18:25:35 +0900

    feat: 2 x n 타일링

---
+++ src/baekjoon/programmers/level2/이곱하기n타일링/Solution.java
@@ -0,0 +1,19 @@
+package baekjoon.programmers.level2.이곱하기n타일링;
+
+public class Solution {
+
+    private static final int MOD = 1_000_000_007;
+
+    public int solution(int n) {
+
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+}
