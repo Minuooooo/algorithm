dc7079a814a583b4fe52998ad25d65bc7f2cf00e
Minuooooo <mmw7741@gmail.com>
2024-03-19 17:56:16 +0900

    feat: P17626 Four Squares

---
+++ src/baekjoon/dynamic/P17626FourSquares/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.dynamic.P17626FourSquares;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1];
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j]);
+            }
+            dp[i]++;
+        }
+        System.out.println(dp[n]);
+    }
+}
